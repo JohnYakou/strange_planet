b3e8a858b659f8a986028945247d8d07493d57d3
John <jyakou@gmx.fr>
2023-08-09 17:35:36 +0200

    Drawing game score - 09/08/2023 - 17:35

--- script.js
+++ script.js
@@ -153,15 +153,23 @@ window.addEventListener('load', function(){
             this.game = game;
             this.fontSize = 25;
             this.fontFamily = "Helvitica";
-            this.color = "yellow";
+            this.color = "white";
         }
 
         draw(context){
-            // ammo
+            context.save();
             context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor ="black";
+            context.font = this.fontSize + "px " + this.fontFamily;
+            // score
+            context.fillText("score : " + this.game.score, 20, 40);
+            // ammo
             for(let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            context.restore();
         }
     }
 
@@ -182,6 +190,8 @@ window.addEventListener('load', function(){
             this.ammoTimer = 0;
             this.ammoInterval = 500;
             this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 10;
         }
         
         update(deltaTime){
@@ -208,6 +218,7 @@ window.addEventListener('load', function(){
                         if(enemy.lives < 0){
                             enemy.markedForDeletion = true;
                             this.score += enemy.score;
+                            if(this.score > this.winningScore) this.gameOver = true;
                         }
                     }
                 })
