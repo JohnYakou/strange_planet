c905a3575ab9d91f978547e8bc9499bd6789b514
John <jyakou@gmx.fr>
2023-08-09 14:23:12 +0200

    Creating projectiles - 09/08/2023 - 14:23

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ window.addEventListener('load', function(){
             this.game = game;
             // e => Une caractéristique particulière de la fonction flèche est que ce mot-clé à l'intérieur de la fonction flèche représente toujours les objets dans lesquels la fonction flèche est définie.
             window.addEventListener("keydown", e => {
-                console.log(e.key);
+                // console.log(e.key);
                 if(((e.key === "ArrowUp") ||
                     (e.key === "ArrowDown")
                 ) && this.game.keys.indexOf(e.key) === -1){
                     this.game.keys.push(e.key);
+                // le vide entre les guillemets c'est la touche "espace" 
+                }else if(e.key === " "){
+                    this.game.player.shootTop();
                 }
             });
 
@@ -26,13 +29,31 @@ window.addEventListener('load', function(){
                     // La méthode splice() modifie le contenu d'un tableau en supprimant ou en remplaçant les éléments existants.
                     this.game.keys.splice(this.game.keys.indexOf(e.keys), 1);
                 }
-                console.log(this.game.keys);
+                // console.log(this.game.keys);
             });
         }
     }
 
     class Projectile{
-
+        constructor(game, x, y){
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3;
+            this.markedForDeletion = false;
+        }
+        
+        update(){
+            this.x += this.speed;
+            if(this.x > this.game.width * 0.8) this.markedForDeletion = true;
+        }
+        
+        draw(context){
+            context.fillStyle = "yellow";
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     class Particle{
@@ -49,7 +70,9 @@ window.addEventListener('load', function(){
             this.y = 100;
             this.speedY = 0;
             this.maxSpeed = 3;
+            this.projectiles = [];
         }
+        
         update(){
             // La méthode includes() permet de déterminer si un tableau contient une valeur et renvoie true si c'est le cas, false sinon.
             if(this.game.keys.includes("ArrowUp")) this.speedY = -this.maxSpeed;
@@ -57,9 +80,30 @@ window.addEventListener('load', function(){
             // Sans  ce else, le perso ne s'arrête pas.
             else this.speedY = 0;
             this.y += this.speedY;
+            // handle projectiles
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            });
+
+            // La méthode filter() crée et retourne un nouveau tableau contenant tous les éléments du tableau d'origine qui remplissent une condition déterminée par la fonction callback.
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
         }
+        
         draw(context){
+            context.fillStyle = "green";
             context.fillRect(this.x, this.y, this.width, this.height);
+
+            this.projectiles.forEach(projectile => {
+                projectile.draw(context);
+            });
+        }
+
+        shootTop(){
+            if(this.game.ammo > 0){
+                this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 30));
+                // A chaque que je tire, je tire une munition. J'en n'ai 20 au début (voir le constructor de la class Game).
+                this.game.ammo--;
+            }
         }
     }
 
@@ -87,10 +131,13 @@ window.addEventListener('load', function(){
             this.player = new Player(this);
             this.input = new InputHandler(this);
             this.keys = [];
+            this.ammo = 20;
         }
+        
         update(){
             this.player.update();
         }
+      
         draw(context){
             this.player.draw(context);
         }
@@ -107,5 +154,6 @@ window.addEventListener('load', function(){
         // requestAnimationFrame indique au navigateur que nous souhaitons réaliser une animation et lui demande d'appeler une fonction spécifique pour mettre à jour l'animation avant la prochaine répétition.
         requestAnimationFrame(animate);
     }
+    
     animate();
 });
