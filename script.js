4d31dc0ba831a2b70d0d8e0f4a07f68314226a53
John <jyakou@gmx.fr>
2023-08-09 11:53:15 +0200

    Player and Game Objects + Animation Loop - 09/08/2023 - 11:53

--- script.js
+++ script.js
@@ -6,4 +6,78 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext("2d");
     canvas.width = 500;
     canvas.height = 500;
+
+    class InputHandler{
+
+    }
+
+    class Projectile{
+
+    }
+
+    class Particle{
+
+    }
+
+    class Player{
+        // CONSTRUCTOR est une méthode spéciale de la class JS. Lorsque j'appelle la classe JS en utilisant le mot-clé new, le constructeur crée un objet JS vierge et lui attribue des propriétés et des valeurs dans le blueprint qu'il contient.
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Enemy{
+
+    }
+
+    class Layer{
+
+    }
+
+    class Background{
+
+    }
+
+    class UI{
+
+    }
+
+    class Game{
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            // crée une nouvelle instance de la class Player
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+
+    // animation loop
+    function animate(){
+        // permet d'éviter au rectangle noir (futur perso) de s'étirer
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        // requestAnimationFrame indique au navigateur que nous souhaitons réaliser une animation et lui demande d'appeler une fonction spécifique pour mettre à jour l'animation avant la prochaine répétition.
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
