57dbdc1cc902f3a79935db1c95bfc4b8c63fcd62
John <jyakou@gmx.fr>
2023-08-10 21:44:55 +0200

    Fire effect animation - 10/08/2023 - 21:44

--- script.js
+++ script.js
@@ -355,7 +355,12 @@ window.addEventListener('load', function(){
             this.x = x;
             this.y = y;
             this.frameX = 0;
+            this.spriteWidth = 200;
             this.spriteHeight = 200;
+            this.width = this.spriteWidth
+            this.height = this.spriteHeight
+            this.x = x - this.width * 0.5;
+            this.y = y - this.height * 0.5;
             this.fps = 30;
             this.timer = 0;
             this.interval = 1000 / this.fps;
@@ -385,16 +390,14 @@ window.addEventListener('load', function(){
         constructor(game, x, y){
             super(game, x, y);
             this.image = document.getElementById("smokeExplosion");
-            this.spriteWidth = 200;
-            this.width = this.spriteWidth
-            this.height = this.spriteHeight
-            this.x = x - this.width * 0.5;
-            this.y = y - this.height * 0.5;
         }
     }
 
     class fireExplosion extends Explosion{
-        
+        constructor(game, x, y){
+            super(game, x, y);
+            this.image = document.getElementById("fireExplosion");
+        }
     }
 
     class UI{
@@ -584,7 +587,11 @@ window.addEventListener('load', function(){
 
         addExplosion(enemy){
             const randomize = Math.random();
-            if(randomize < 1) this.explosions.push(new smokeExplosion(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
+            if(randomize < 0.5){
+                this.explosions.push(new smokeExplosion(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
+            }else{
+                this.explosions.push(new fireExplosion(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
+            }
         }
 
         // rect = rectangle
